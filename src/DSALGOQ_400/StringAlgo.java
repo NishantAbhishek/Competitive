51511634c7828be1c35b97d5c88f224fb823b950
Nishant <Nishant2548@gmail.com>
2021-06-05 13:06:05 +0530

    String Algo

--- src/DSALGOQ_400/StringAlgo.java
+++ src/DSALGOQ_400/StringAlgo.java
@@ -2,7 +2,9 @@ package DSALGOQ_400;
 
 public class StringAlgo {
     public static void main(String[] args) {
-
+//        System.out.println(isRotation("AACD","ACDA"));
+//        findAllDuplicates("geeksforgeeks");
+        longestPalin("aaaabbaa");
     }
 
     //reverse String
@@ -18,7 +20,7 @@ public class StringAlgo {
         }
     }
 
-    int isPlaindrome(String S) {
+    static int isPlaindrome(String S) {
         int initial = 0;
         int end = S.length()-1;
         while (initial<end){
@@ -29,6 +31,52 @@ public class StringAlgo {
             end--;
         }
         return 1;
-
     }
+
+    //Whether strA is rotation of strB
+    private static boolean isRotation(String strA,String strB){
+        String strC = strA+strA;
+
+        if(strC.contains(strB)){
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    static int NO_OF_CHARS = 256;
+    //hashing
+    static void findAllDuplicates(String a){
+//        System.out.println((int)a.charAt(1));
+        int[] chars = new int[NO_OF_CHARS];
+
+        for(int i = 0;i<a.length();i++){
+            chars[a.charAt(i)]=chars[a.charAt(i)]+1;
+        }
+
+        for(int i = 0;i<chars.length;i++){
+            if(chars[i]>1){
+                System.out.println((char) i+" count:- "+chars[i]);
+            }
+        }
+    }
+
+    static int longestPalin(String s){
+        String maxPa = "";
+        for(int i = 0;i<s.length();i++){
+            for(int k = i;k<s.length();k++){
+                String sub = s.substring(i,k+1);
+                System.out.println(sub);
+                int isPalin = isPlaindrome(sub);
+                if(isPalin==1){
+                    if(sub.length()>maxPa.length()){
+                        maxPa = sub;
+                    }
+                }
+            }
+        }
+        return maxPa.length();
+    }
+
+
 }
