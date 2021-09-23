b9807a39599d443f7fda1e91719d3c8631aa51a5
nishant <nishant2548@gmail.com>
2021-09-23 22:04:48 +0530

    Revision

---
+++ src/September/Day6.java
@@ -0,0 +1,14 @@
+package September;
+
+public class Day6 {
+    //9,
+    public static void main(String[] args) {
+        int arr[] = {9,10,1,2,3,4,8,0,0,0,0,0,0,0,1};
+    }
+
+    public void nextPermutation(int[] nums) {
+        
+
+    }
+
+}
