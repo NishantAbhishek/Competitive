9b8d11af8f8742f332be33e33791524bdeaaac74
nishant <nishant2548@gmail.com>
2021-10-27 22:38:43 +0530

    Day11

---
+++ src/October/Day10.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int mul(int num);
+
+int main(){
+
+    int data[] = {-1,2,4,-3,5,2,-5,2};
+    int size = 8;
+
+    int maxSumSub = 0;
+    for(int  i = 0;i<size;i++){
+        if(data[i]>maxSumSub){
+            
+        }
+    }
+    return 0;
+}
+
+int mul(int num){
+    if(num==0){
+        return 1;
+    }else{
+        return num * mul(num-1);
+    }
+}
+
